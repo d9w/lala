455209c1aa6d3d1769a06877ed59c2bf40515efe
Dennis Wilson <dennis@d9w.xyz>
2017-06-08 12:33:00 +0200

    start of da-stdp conditioning experiment

---
+++ cond/da-stdp.jl
@@ -0,0 +1,102 @@
+# DA-STDP
+# Dennis G Wilson, June 8, 2017
+# Adapted from daspnet.m by Eugene M. Izhikevich
+# https://www.izhikevich.org/publications/daspnet.m
+
+srand(1234)
+
+M = 100   # number of post-synaptic connections per neuron
+D = 1     # conduction delay (timesteps between pre and post firing)
+Ne = 800  # excitatory neurons
+Ni = 200  # inhibitory neurons
+N = Ne+Ni # total number
+a = [0.02*ones(Ne,1); 0.1*ones(Ni,1)] # TODO: unused?
+d = [8*ones(Ne,1); 2*ones(Ni,1)] # TODO: unused?
+sm = 4    # maximal synaptic strength
+
+post = Int64.(ceil([N*rand(Ne,M);Ne*rand(Ni,M)])) # post-synaptic mapping
+# s = [0.25*randn(Ne,M)+1.0;0.25*randn(Ni,M)-1.0]      # synaptic weights
+s = [ones(Ne,M); -1.0*ones(Ni,M)]      # synaptic weights
+sd = zeros(N,M)                   # their derivatives
+#delays = Array{Any}(N,D)         # unused, replace with static delay
+del = 1:M
+pre = Array{Array{Int64}}(N)
+aux = Array{Array{Int64}}(N)
+
+for i = 1:N
+    pre[i] = find((post.==i) & (s.>0)) # pre-synaptic mapping
+    aux[i] = Int64.(N*(D-1-ceil(ceil(pre[i]/N)/(M/D)))+1+mod(pre[i]-1,N)) # TODO: unsure
+end
+
+secs = 24         # the duration of experiment [s]
+T = 1000          # timesteps per sec
+DA = 0            # level of dopamine above the baseline (use neuron-specific)
+rew = Array{Int64}(0)          # reward instances (use array with zero norm)
+
+STDP = zeros(N,N+1+D)
+v = -65*ones(N,1) # membrane potential
+u = 0.2.*v        # membrane recovery variable
+firings = [-D 0]  # spike timings
+
+# TODO: replace this with instrumental coding
+n1 = 1            # presynaptic neuron
+syn = 1           # the synapse number to the postsynaptic neuron
+n2 = post[n1,syn] # postsynaptic neuron
+s[n1,syn] = 1.0   # start with 0 value
+
+interval = 40     # the coincidence interval for n1 and n2
+n1f = [-100]      # the last spike of n1
+n2f = [];         # the last spike of n2
+shist = Array{Float64}((secs+1)*T,2) # history of s over time
+
+for sec=0:secs                      # simulation of 1 minute
+    for t=1:T                       # simulation of 1 sec
+        tinp = 13*(rand(N,1)-0.5)   # random thalamic input
+        fired = find(v.>=30);       # indices of fired neurons
+        if length(fired)>0
+            v[fired] = -65            # reset v
+            u[fired] = u[fired] + d[fired] # reset u
+            STDP[fired, t+D] = 0.1    # increase STDP
+            for k=1:length(fired)
+                sd[pre[fired[k]]] += STDP[N*t+aux[fired[k]]]
+            end
+            firings = [firings; Int64.([t*ones(length(fired),1) fired])]
+            k = size(firings,1)
+            while firings[k,1]>t-D
+                ind = post[firings[k,2],del]
+                tinp[ind] = tinp[ind]+s[firings[k,2], del]
+                sd[firings[k,2],del] = sd[firings[k,2],del]-1.5*STDP[ind,t+D]
+                k = k-1;
+            end
+        end
+        v = v+0.5*((0.04*v+5).*v+140-u+tinp);    # for numerical stability
+        v = v+0.5*((0.04*v+5).*v+140-u+tinp);    # for numerical stability
+        u = u+a.*(0.2*v-u);                      # time step is 0.5 ms
+        STDP[:,t+D+1] = 0.95*STDP[:,t+D];        # tau = 20 ms
+
+        DA = DA*0.995;
+        if (mod(t,10) == 0)
+            s[1:Ne,:] = max(0,min(sm,s[1:Ne,:]+(0.002+DA)*sd[1:Ne,:]));
+            sd = 0.99*sd;
+        end;
+        if any(fired .== n1)
+            append!(n1f, [sec*T+t])
+        end
+        if any(fired .== n2)
+            append!(n2f, [sec*T+t])
+            if (sec*T+t-n1f[end]<interval) & (n2f[end]>n1f[end])
+                append!(rew, [sec*T+t+T+ceil(2*T*rand())])
+            end
+        end
+        if any(rew .== sec*T+t)
+            DA = DA+0.5;
+        end
+        shist[secs*T+t,:] = [s[n1, syn] sd[n1, syn]]
+    end
+    STDP[:,1:D+1] = STDP[:,(N+1):(N+1+D)];
+    ind  =  find(firings[:,1] .> N+1-D);
+    firings = [-D 0; [firings[ind,1]-N firings[ind,2]]];
+    println(sec, rew)
+end
+
+nothing
